75c89104bfe9ff382214cc0258c6a6114a69e6c7
Ian Thomas <ianthomas23@gmail.com>
2023-06-10 16:33:37 +0100

    Take 7

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,12 @@
 set -ex
 
 EXTRAS=""
+
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]
 then
-    EXTRAS='--config-settings=setup-args=--cross-file="$SRC_DIR/meson-cross.ini"'
+    EXTRAS="--config-settings=setup-args=--cross-file=$PWD/meson-cross.ini"
 fi
+
+ls -l
 
 $PYTHON -m pip install . -vv --no-build-isolation $EXTRAS
