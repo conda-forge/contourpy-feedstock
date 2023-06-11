c24f171916f5a927cb50211e1c9a6d99e5310615
Ian Thomas <ianthomas23@gmail.com>
2023-06-11 16:57:03 +0100

    Expt

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,22 +5,27 @@ if [[ "${target_platform}" == "osx-arm64" ]]
 then
     # Cross-compilation using similar approach to scikit-image.
     # Also see https://conda-forge.org/blog/posts/2020-10-29-macos-arm64/ about MESON_ARGS.
-    mkdir builddir
+#    mkdir builddir
 
-    echo ${MESON_ARGS}
+#    echo ${MESON_ARGS}
 
-     # Temporary fix for finding pybind11 for cross-compiling until meson 1.2.0 is released.
-    MESON_ARGS="$MESON_ARGS --cross-file=$PWD/meson-cross.ini"
+#    MESON_ARGS="$MESON_ARGS --cross-file=$PWD/meson-cross.ini"
 
-    echo ${MESON_ARGS}
+#    echo ${MESON_ARGS}
 
-    ${PYTHON} $(which meson) setup ${MESON_ARGS} builddir
+#    ${PYTHON} $(which meson) setup ${MESON_ARGS} builddir
 
-    ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
-    ${PYTHON} -m pip install dist/contourpy*.whl
+#    ${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+#    ${PYTHON} -m pip install dist/contourpy*.whl
+
+
+    cat $BUILD_PREFIX/meson_cross_file.txt
 
     # This may not be required...
     #export ARCHFLAGS="-arch arm64"
+
+    # Second cross-file is a temporary fix for finding pybind11 when cross-compiling until meson 1.2.0 is released.
+    $PYTHON -m pip install . -vv --no-build-isolation --config-settings=setup-args=--cross-file=$BUILD_PREFIX/meson_cross_file.txt --config-settings=setup-args=--cross-file=$PWD/meson-cross.ini
 else
     $PYTHON -m pip install . -vv --no-build-isolation
 fi
