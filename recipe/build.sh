6cb17153608f0e47cc79cc2c1dd6a90e9c7dcace
Ian Thomas <ianthomas23@gmail.com>
2023-06-11 14:57:50 +0100

    Take 2

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,22 @@ set -ex
 if [[ "${target_platform}" == "osx-arm64" ]]
 then
     export ARCHFLAGS="-arch arm64"
-    env
-    which python
-    $PYTHON --version
-    $PYTHON -m pip install . -vv --no-build-isolation --config-settings=setup-args=--cross-file=$PWD/meson-cross.ini
+
+    # From scipy-feedstock:
+    # meson-python already sets up a -Dbuildtype=release argument to meson, so
+    # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
+    MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+
+     # Temporary fix for finding pybind11 for cross-compiling until meson 1.2.0 is released.
+    MESON_ARGS_REDUCED="$MESON_ARGS_REDUCED --cross-file=$PWD/meson-cross.ini"
+
+    #env
+    #which python
+    #$PYTHON --version
+
+    # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+    $PYTHON -m build -w -n -x -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=}
+    $PYTHON -m pip install dist/contourpy*.whl
 else
-    $PYTHON -m pip install . -vv --no-build-isolation $SUFFIX
+    $PYTHON -m pip install . -vv --no-build-isolation
 fi
