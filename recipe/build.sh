cac60d3db5bab7a885e04039fa2d1de8d525a1d0
Ian Thomas <ianthomas23@gmail.com>
2023-06-11 10:10:24 +0100

    Alternative approach

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-EXTRAS=""
-
-if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]
+if [[ "${target_platform}" == "osx-arm64" ]]
 then
-    EXTRAS="--config-settings=setup-args=--cross-file=$PWD/meson-cross.ini"
+    export ARCHFLAGS="-arch arm64"
+    env
+    which python
+    $PYTHON --version
+    $PYTHON -m pip install . -vv --no-build-isolation --config-settings=setup-args=--cross-file=$PWD/meson-cross.ini
+else
+    $PYTHON -m pip install . -vv --no-build-isolation $SUFFIX
 fi
-
-ls -l
-
-$PYTHON -m pip install . -vv --no-build-isolation $EXTRAS
